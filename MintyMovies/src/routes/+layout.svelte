c19ee47d5d2f67e0d975e2ccca55bc199318af3e
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-06-15 14:37:38 +0200

    Added style and cleaned out pre text

--- MintyMovies/src/routes/+layout.svelte
+++ MintyMovies/src/routes/+layout.svelte
@@ -1,11 +1,27 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import '../lib/css/style.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Pangolin&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
-{@render children()}
+<section>
+	<nav>
+		<a href="./">Home</a>
+		<a href="./">som</a>
+	</nav>
+	<div id="content">
+		{@render children()}
+	</div>
+</section>
