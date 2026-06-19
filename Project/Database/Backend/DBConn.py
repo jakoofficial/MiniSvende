52dc1ec9da1cff7e52bd837b380bb7b0b4d34594
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-06-19 10:51:27 +0200

    Added few endpoints and det sqlalchemy classes/tables to the database, i also added relastions between the user and their signin and recieve the correct data

--- Project/Database/Backend/DBConn.py
+++ Project/Database/Backend/DBConn.py
@@ -1,8 +1,9 @@
-from sqlalchemy import String, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy import Boolean, String, Integer, ForeignKey, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
 
 # Connection
-engine = create_engine("mysql+pymysql://root:admin@mysql:3306/minty", echo=True)
+# engine = create_engine("mysql+pymysql://root:admin@mysql:3316/minty", echo=True)
+engine = create_engine("mysql+pymysql://root:admin@0.0.0.0:3316/minty", echo=True)
 
 class Base(DeclarativeBase):
     pass
@@ -13,6 +14,58 @@ class test(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
+
+class UserSession(Base):
+    __tablename__= "user_session"
+
+    session_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    session_key: Mapped[str] = mapped_column(String(50))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_signin.signin_id"))
+
+class UserSignin(Base):
+    __tablename__= "user_signin"
+    
+    signin_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    # user_id: Mapped[int] = mapped_column(Integer)
+    password: Mapped[str] = mapped_column(String(60))
+
+class UserProfile(Base):
+    __tablename__= "user_profile"
+
+    user_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    signin_id: Mapped[int] = mapped_column(ForeignKey("user_signin.signin_id"))
+    username: Mapped[str] = mapped_column(String(32))
+    signin: Mapped["UserSignin"] = relationship()
+
+
+class Subscriptions(Base):
+    __tablename__= "subscriptions"
+    
+    sub_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_profile.user_id"))
+    subscription: Mapped[bool] = mapped_column(Boolean, default=False)
+
+class Movies(Base):
+    __tablename__= "movies"
+
+    movie_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    movie_title: Mapped[str] = mapped_column(String(100))
+    movie_desc: Mapped[str] = mapped_column(String(155))
+    movie_link: Mapped[str] = mapped_column(String(250))
+    premium: Mapped[bool] = mapped_column(Boolean, default=False)
+
+class Categories(Base):
+    __tablename__= "categories"
+
+    category_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    category_title: Mapped[str] = mapped_column(String(50))
+
+class CategoryMovie(Base):
+    __tablename__= "category_movie"
+
+    catmov_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.category_id"))
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.movie_id"))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- Project/Database/main.py
+++ Project/Database/main.py
@@ -1,7 +1,6 @@
-from .Backend.DBConn import *
-from .Backend.API.EndPoints import *
+from Backend.DBConn import *
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -17,7 +16,34 @@ def get_db():
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/getItem")
-def read_item(db: Session = Depends(get_db)):
-    testobj = db.query(test).first()
-    return testobj.name
+@app.get("/getUser")
+def read_item(userID: int, db: Session = Depends(get_db)):
+    profile = db.query(UserProfile).filter(UserProfile.user_id == userID).first()
+
+    if not profile:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return {
+        "user_id: ": profile.user_id,
+        "username": profile.username
+    }
+
+@app.post("/signin")
+def user_signin(username, password, db: Session = Depends(get_db)):
+    user = db.query(UserProfile).filter(UserProfile.username == username).first()
+    if user and user.signin.password == password:
+        sess = UserSession()
+        sess.session_key = "000001"
+        sess.user_id = user.user_id
+        db.add(sess)
+        db.commit()
+        db.refresh(sess)
+
+        u = {"name": user.username}
+
+        return [u, sess]
+    
+    return "No user found"
+    # signin = db.query
+
+    pass
